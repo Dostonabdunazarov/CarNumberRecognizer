326b5f8b3823c66d73b87cea21e54b852b0b5d2b
AbdunazarovD <abdunazarov.doston@mail.com>
2022-05-13 10:28:51 +0500

    add code

---
+++ CarNumberRecognize/Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace CarNumberRecognize
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent() {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.menuToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.открытьКартинкуToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.menuStrip1.SuspendLayout();
+            this.toolStrip1.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1067, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // menuToolStripMenuItem
+            // 
+            this.menuToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.открытьКартинкуToolStripMenuItem});
+            this.menuToolStripMenuItem.Name = "menuToolStripMenuItem";
+            this.menuToolStripMenuItem.Size = new System.Drawing.Size(59, 24);
+            this.menuToolStripMenuItem.Text = "Файл";
+            // 
+            // открытьКартинкуToolStripMenuItem
+            // 
+            this.открытьКартинкуToolStripMenuItem.Name = "открытьКартинкуToolStripMenuItem";
+            this.открытьКартинкуToolStripMenuItem.Size = new System.Drawing.Size(150, 26);
+            this.открытьКартинкуToolStripMenuItem.Text = "Открыть";
+            this.открытьКартинкуToolStripMenuItem.Click += new System.EventHandler(this.открытьКартинкуToolStripMenuItem_Click);
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripButton1});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 28);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(1067, 27);
+            this.toolStrip1.TabIndex = 1;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripButton1
+            // 
+            this.toolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton1.Image")));
+            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton1.Name = "toolStripButton1";
+            this.toolStripButton1.Size = new System.Drawing.Size(150, 24);
+            this.toolStripButton1.Text = "Распознать номера";
+            this.toolStripButton1.Click += new System.EventHandler(this.toolStripButton1_Click);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.panel1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.pictureBox1, 1, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 55);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1067, 588);
+            this.tableLayoutPanel1.TabIndex = 2;
+            // 
+            // panel1
+            // 
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(3, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(527, 582);
+            this.panel1.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(536, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(528, 582);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1067, 643);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Распознавания номеров";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem menuToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem открытьКартинкуToolStripMenuItem;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton toolStripButton1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ CarNumberRecognize/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
+
+namespace CarNumberRecognize
+{
+    public partial class Form1 : Form
+    {
+        private NumberPlateRecognizer plateRecognizer;
+        private Point startPoint;
+        private Mat inputImage;
+        public Form1() {
+            InitializeComponent();
+        }
+        void ProcessImage(IInputOutputArray image) {
+            List<IInputOutputArray> licenseImagePlateList = new List<IInputOutputArray>();
+            List<IInputOutputArray> filteredLicensePlateImageList = new List<IInputOutputArray>();
+            List<RotatedRect> licenseBoxList = new List<RotatedRect>();
+            List<string> recognizedPlates = plateRecognizer.DetectLicensePlates(image,
+                licenseImagePlateList, filteredLicensePlateImageList, licenseBoxList);
+            panel1.Controls.Clear();
+            startPoint = new Point(10, 10);
+            for (int i = 0; i < recognizedPlates.Count; i++) {
+                Mat dest = new Mat();
+                CvInvoke.VConcat(licenseImagePlateList[i], filteredLicensePlateImageList[i], dest);
+                AddLabelAndImage($"Номер: {recognizedPlates[i]}", dest);
+            }
+            Image<Bgr, byte> outputImage = inputImage.ToImage<Bgr, byte>();
+            foreach(RotatedRect rect in licenseBoxList) {
+                PointF[] v = rect.GetVertices();
+                PointF prevPoint = v[0];
+                PointF firstPoint = prevPoint;
+                PointF nextPoint = prevPoint;
+                PointF lastPoint = nextPoint;
+                for(int i = 0; i < v.Length; i++) {
+                    nextPoint = v[i];
+                    CvInvoke.Line(outputImage, Point.Round(prevPoint), Point.Round(nextPoint), new MCvScalar(0, 0, 255), 5,
+                        LineType.EightConnected, 0);
+                    prevPoint = nextPoint;
+                    lastPoint = prevPoint;
+
+                }
+                CvInvoke.Line(outputImage, Point.Round(lastPoint), Point.Round(firstPoint), new MCvScalar(0, 0, 255), 5,
+                    LineType.EightConnected, 0);
+            }
+        }
+        void AddLabelAndImage(string labelText, IInputArray image) {
+            Label label = new Label();
+            label.Text = labelText;
+            label.Width = 100;
+            label.Height = 40;
+            label.Location = startPoint;
+            startPoint.Y += label.Height;
+            panel1.Controls.Add(label);
+            PictureBox box = new PictureBox();
+            Mat m = image.GetInputArray().GetMat();
+            box.ClientSize = m.Size;
+            box.Image = m.Bitmap;
+            box.Location = startPoint;
+            startPoint.Y += box.Height + 10;
+            panel1.Controls.Add(box);
+        }
+
+        private void открытьКартинкуToolStripMenuItem_Click(object sender, EventArgs e) {
+            try {
+                if(openFileDialog1.ShowDialog() == DialogResult.OK) {
+                    pictureBox1.Image = Image.FromFile(openFileDialog1.FileName);
+                }
+            }
+            catch(Exception ex) {
+                MessageBox.Show(ex.Message, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+            plateRecognizer = new NumberPlateRecognizer(@"E:\CarNumberRecognize\CarNumberRecognize\testdata", "rus");
+        }
+
+        private void toolStripButton1_Click(object sender, EventArgs e) {
+            inputImage = new Mat(openFileDialog1.FileName);
+            UMat um = inputImage.GetUMat(AccessType.ReadWrite);
+            ProcessImage(um);
+        }
+    }
+}
